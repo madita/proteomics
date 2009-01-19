e384d504b2767412c89cbbc52068b130f8cec026
madita <madita.thiemann@gmail.com>
2009-01-19 17:03:47 +0100

    proteomics.pl verändert

--- proteomics.pl
+++ proteomics.pl
@@ -14,7 +14,9 @@ for (my $cnt = 0; $cnt < $#linearr; $cnt++ ) {
 	
 	if ($linearr[$cnt] =~ />/) {
 		$sequenz = merge_seq($cnt);
+		print "$sequenz \n";
 		@tryp = trypsincut($sequenz);
+		for (my $i=0; $i<$#tryp; $i++) {print "$i $tryp[$i]\n";}
 	}	
 
 }
@@ -24,9 +26,9 @@ my ($start) = $_[0];
 my $sequenz = "";
 
  for (my $cnt = ($start+1); $cnt < $#linearr; $cnt++ ) {
-	$sequenz = $sequenz.$linearr[$cnt];
+	$sequenz.= $linearr[$cnt];
 	if ($linearr[$start] =~ />/) {
-		last;
+		return $sequenz;
 	}
  }
 
@@ -34,15 +36,21 @@ my $sequenz = "";
 
 sub trypsincut{
 
-my $start;
-my @tryp;
-$start = $_[0];
- for (my $cnt = $start; $cnt < $#linearr; $cnt++ ) {
-	if ($linearr[$start] =~ />/) {
-		last;
+my ($sequenz) = $_[0];
+$sequenz =~ s/K/K1/g;
+$sequenz =~ s/RP/2/g;
+$sequenz =~ s/R/R3/g;
+my (@tryp) = split("1", $sequenz);
+
+  for (my $cnt = 0; $cnt < $#tryp; $cnt++ ) {
+	$tryp[$cnt] =~ s/2/RP/g;
+	if ($tryp[$cnt] =~ /3/) {
+		my (@temp) = split("3", $tryp[$cnt]);
 	}
- }
+	
+  }
 
 return @tryp;
 }
+
 
