b5bd528c2c9aca320bc09d7c9ccc427a3e02fe1c
Marten <mar@earth.(none)>
2009-01-19 20:04:46 +0100

    sub getCalcMasses zugefuegt

--- proteomics.pl
+++ proteomics.pl
@@ -4,6 +4,7 @@ use strict;
 
 my $sequenz;
 my @tryp;
+my @masses;
 
 local $/ = "\n";
 open(FILE, "<human") || die ("Fehler beim Öffnen der Datei \n");					
@@ -15,6 +16,7 @@ for (my $cnt = 0; $cnt < $#linearr; $cnt++ ) {
 	if ($linearr[$cnt] =~ />/) {
 		$sequenz = merge_seq($cnt);
 		@tryp = trypsincut($sequenz);
+		@masses = getCalcMasses(@tryp);
 	}	
 
 }
@@ -49,6 +51,40 @@ my (@tryp) = split("1", $sequenz);
   }
 
 return @tryp;
+}
+
+sub getCalcMasses{
+	my @tryp=$_[0];
+	my %asMasses=(	"A" => 71.07884,
+					"C" => 103.14484,
+					"D" => 115.08864,
+					"E" => 129.11552,
+					"F" => 147.1766,
+					"G" => 57.05196,
+					"H" => 137.1412,
+					"I" => 113.15948,
+					"K" => 128.17416,
+					"L" => 113.15948,
+					"M" => 131.1986,
+					"N" => 114.10392,
+					"P" => 97.11672,
+					"Q" => 128.1308,
+					"R" => 156.18764,
+					"S" => 87.07824,
+					"T" => 101.10512,
+					"V" => 99.1326,
+					"W" => 186.21328,
+					"Y" => 163.176,
+					"X" => 110,
+					"Z" => 110,
+					"B" => 110			);
+	my @as;	my $tmp;	my @calcMasses;
+	
+	for(my $i=0;$i<=$#tryp;$i++){
+		@as=split("", $tryp[$i]);
+		for(my $j=0;$j<=$#as;$j++){$calcMasses[$i]+=$asMasses{$as[$j]};}
+	}
+	return @calcMasses;
 }
 
 
